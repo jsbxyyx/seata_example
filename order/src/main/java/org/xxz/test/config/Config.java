3af2b71bb95575f45a128cdec2afba4efadceed5
jsbxyyx <jsbxyyx@163.com>
2019-07-01 15:12:45 +0800

    Update Config.java

--- order/src/main/java/org/xxz/test/config/Config.java
+++ order/src/main/java/org/xxz/test/config/Config.java
@@ -1,8 +1,13 @@
 package org.xxz.test.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import io.seata.rm.datasource.DataSourceProxy;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import javax.sql.DataSource;
 
 /**
  * @author jsbxyyx
@@ -14,6 +19,17 @@ public class Config {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean(destroyMethod = "close", initMethod = "init")
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DruidDataSource druidDataSource() {
+        return new DruidDataSource();
+    }
+
+    @Bean("dataSource")
+    public DataSource dataSource(DruidDataSource druidDataSource) {
+        return new DataSourceProxy(druidDataSource);
     }
 
 }
