4af1e6505ee1f4c8e255179411423c2719962f7b
jsbxyyx <jsbxyyx@163.com>
2019-09-28 09:35:36 +0800

    add eureka configuration

--- test1/src/main/java/org/xxz/test/Test1Application.java
+++ test1/src/main/java/org/xxz/test/Test1Application.java
@@ -2,12 +2,14 @@ package org.xxz.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author jsbxyyx
  * @since
  */
 @SpringBootApplication(scanBasePackages = {"org.xxz"})
+@EnableDiscoveryClient
 public class Test1Application {
 
     public static void main(String[] args) {
