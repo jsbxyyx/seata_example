2cc3efdd5832f418bf35e94b8af365467559c57a
jsbxyyx <jsbxyyx@163.com>
2019-10-15 13:59:39 +0800

    add mybatis plus

--- test/src/main/java/org/xxz/test/config/MybatisConfig.java
+++ test/src/main/java/org/xxz/test/config/MybatisConfig.java
@@ -4,7 +4,10 @@ import io.seata.rm.datasource.DataSourceProxy;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.transaction.SpringManagedTransactionFactory;
+import org.springframework.beans.BeansException;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
@@ -14,16 +17,23 @@ import java.util.Properties;
 
 @Configuration
 @AutoConfigureAfter(Config.class)
-public class TkMybatisConfig {
+public class MybatisConfig implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
 
     @Bean
     public SqlSessionFactory sqlSessionFactory(DataSourceProxy dataSourceProxy) throws Exception {
+        // tk mybatis
         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setConfiguration(mybatisConfig());
+
+        // mybatis-plus
+//        com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean factoryBean = new com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean();
+
         factoryBean.setDataSource(dataSourceProxy);
         factoryBean.setMapperLocations(new PathMatchingResourcePatternResolver()
                 .getResources("classpath*:/mapper/*.xml"));
         factoryBean.setTransactionFactory(new SpringManagedTransactionFactory());
-        factoryBean.setConfiguration(mybatisConfig());
         return factoryBean.getObject();
     }
 
@@ -41,7 +51,8 @@ public class TkMybatisConfig {
     @Bean
     public org.mybatis.spring.mapper.MapperScannerConfigurer mapperScannerConfigurer() {
         org.mybatis.spring.mapper.MapperScannerConfigurer conf = new org.mybatis.spring.mapper.MapperScannerConfigurer();
-        conf.setBasePackage("org.xxz.test.dao");
+        String basePackage = applicationContext.getEnvironment().getProperty("mybatis.mapper.basePackage");
+        conf.setBasePackage(basePackage);
         conf.setSqlSessionFactoryBeanName("sqlSessionFactory");
         return conf;
     }
@@ -54,4 +65,8 @@ public class TkMybatisConfig {
         return configuration;
     }
 
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
 }
